1fc691aead81c0c790dc501d81d2a031664b4378
Baccan <gustavobaccang@gmail.com>
2019-06-24 00:44:48 +0000

    Mongoose connection

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+// Iniciando App
 const app = express()
 const port = 3001
 
+// Iniciando DB
+mongoose.connect('mongodb://localhost:27017/node-rocketseat-starter', {
+  useNewUrlParser: true
+})
+
+// Rotas
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Api app listening on port ${port}!`))
