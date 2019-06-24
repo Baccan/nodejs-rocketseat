c25265f063f7dba166bc0efa70a0ad02fb843d67
Baccan <gustavobaccang@gmail.com>
2019-06-24 00:58:14 +0000

    Model de produto e adição ao Banco

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 // Iniciando App
 const app = express()
@@ -9,7 +10,18 @@ const port = 3001
 mongoose.connect('mongodb://localhost:27017/node-rocketseat-starter', {
   useNewUrlParser: true
 })
+requireDir('./src/models')
+
+const Product = mongoose.model('Product');
 
 // Rotas
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'https://github.com/facebook/react-native'
+  })
+
+  res.send('Hello World!')
+})
 app.listen(port, () => console.log(`Api app listening on port ${port}!`))
