0ce3cb74209d356ba6db86c65b48d401bf90066b
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-06-24 09:21:14 -0300

    CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,15 +3,41 @@ const mongoose = require('mongoose')
 const Product = mongoose.model('Product');
 
 module.exports = {
+  // List
   async index(req, res) {
     const products = await Product.find()
 
     return res.json(products)
   },
 
+  // Detalhe
+  async show(req, res) {
+    const product = await Product.findById(req.params.id)
+
+    return res.json(product)
+  },
+
+  // Create
   async store(req, res) {
     const product = await Product.create(req.body)
 
     return res.json(product)
-  }
+  },
+
+  // Update
+  async update(req, res) {
+    // Sem o new, retorna o produto antes da atualização
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+
+    return res.json(product)
+  },
+
+  // Delete
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id)
+
+    return res.send()
+  },
 }
